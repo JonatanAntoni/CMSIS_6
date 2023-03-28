0c4b29df0dcd3efda6d9559a951b792540c7a121
Vladimir Marchenko <vladimir.marchenko@arm.com>
2023-03-28 10:21:57 +0200

    Fixed appearance for navigation and tabs

--- doxygen/template/printComponentTabs.js
+++ doxygen/template/printComponentTabs.js
@@ -5,19 +5,19 @@ function tabElement(id, folderName, tabTxt )  {
   this.id = id;                                       // elementID as needed in html;
   this.folderName = folderName;                       // folder name of the component
   this.tabTxt = tabTxt;                               // Text displayed as menu on the web
-  this.currentListItem = '<li id="' + this.id + '" class="current"> <a href="../..' + this.folderName + 'index.html"><span>' + this.tabTxt + '</span></a></li>';
-  this.listItem = '<li id="' + this.id + '"> <a href="../..' + this.folderName + 'index.html"><span>' + this.tabTxt + '</span></a></li>';
+  this.currentListItem = '<li id="' + this.id + '" class="current"> <a href="../' + this.folderName + 'index.html"><span>' + this.tabTxt + '</span></a></li>';
+  this.listItem = '<li id="' + this.id + '"> <a href="../' + this.folderName + 'index.html"><span>' + this.tabTxt + '</span></a></li>';
 };
 
 // array of objects
 var arr = [];
 
 // fill array
- arr.push( new tabElement( "GEN",     "../general",      "General")   );
- arr.push( new tabElement( "CORE_A",  "../core_a/html/", "Core(A)")   );
- arr.push( new tabElement( "CORE_M",  "../core_m/",      "Core(M)")   );
- arr.push( new tabElement( "DRV",     "../driver/",      "Driver")    );
- arr.push( new tabElement( "RTOSv2",  "../rtos2/",       "RTOS v2")   );
+ arr.push( new tabElement( "GEN",     "general/",     "General"));
+ arr.push( new tabElement( "CORE_A",  "core_a/",      "Core(A)"));
+ arr.push( new tabElement( "CORE_M",  "core_m/",      "Core(M)"));
+ arr.push( new tabElement( "DRV",     "driver/",      "Driver"));
+ arr.push( new tabElement( "RTOSv2",  "rtos2/",       "RTOS v2"));
 
 // write tabs
 // called from the header file.
